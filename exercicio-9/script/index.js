ed9d4c7a25d9d3d5858ad6b8504998383be34476
Guyrlam <guyrlamsilvaduque@gmail.com>
2022-06-24 04:43:12 -0300

    feat: add exercise 11

--- exercicio-9/script/index.js
+++ exercicio-9/script/index.js
@@ -10,12 +10,14 @@ button.addEventListener('click', submit);
 
 //functions
 function submit() {
+    
     //variables
     let x = Number(value1.value);
     let z = Number(value1.value);
     let y = Number(value2.value);
     let w = 0;
 
+    //verification and result
     try {
         if (value1.value === '' || value2.value === '') {
             throw new Error(`Certifique-se de preencher ambos os campos com números inteiros!`);
@@ -26,69 +28,49 @@ function submit() {
 
             //sum listener
             case 'sum':
-                try {
-                    if (Number.isInteger(x) === false || Number.isInteger(y) === false || x < 0 || y < 0) {
-                        throw new Error(`[sum] Impossible to sum ${x} + ${y}.`);
-                    } 
-                    result.textContent =(`O valor da soma entre ${value1.value} e ${value2.value} é ${sum(x,y)}. Tente outras operações!`); 
-                } catch (error) {
-                    result.textContent =(error.message);
-                }
+                if (Number.isInteger(x) === false || Number.isInteger(y) === false || x < 0 || y < 0) {
+                    throw new Error(`[sum] Impossible to sum ${x} + ${y}.`);
+                } 
+                result.textContent =(`O valor da soma entre ${value1.value} e ${value2.value} é ${sum(x,y)}. Tente outras operações!`); 
                 break;
 
             //subtraction listener
             case 'subtraction':
-                try {
-                    if (Number.isInteger(x) === false || Number.isInteger(y) === false || x < 0 || y < 0 || y > x) {
-                        throw new Error(`[subtract] Impossible to subtract ${a} - ${b}.`);
-                    }
-                    result.textContent =(`O valor da subtração entre ${value1.value} e ${value2.value} é ${subtraction(x,y)}. Tente outras operações!`);
-                } catch (error) {
-                    result.textContent =(error.message);
+                if (Number.isInteger(x) === false || Number.isInteger(y) === false || x < 0 || y < 0 || y > x) {
+                    throw new Error(`[subtract] Impossible to subtract ${x} - ${y}.`);
                 }
+                result.textContent =(`O valor da subtração entre ${value1.value} e ${value2.value} é ${subtraction(x,y)}. Tente outras operações!`);
                 break;
 
             //multiplication listener
             case 'multiplication':
-                try {
-                    if (Number.isInteger(x) === false || Number.isInteger(y) === false || x < 0 || y < 0){
-                        throw new Error(`[multiply] Impossible to multiply ${a} * ${b}.`);
-                    }
-                    result.textContent =(`O valor da multiplicação entre ${value1.value} e ${value2.value} é ${multiplication(x,y,z)}. Tente outras operações!`);
-                } catch (error) {
-                    result.textContent =(error.message);
+                if (Number.isInteger(x) === false || Number.isInteger(y) === false || x < 0 || y < 0){
+                    throw new Error(`[multiply] Impossible to multiply ${x} * ${y}.`);
                 }
+                result.textContent =(`O valor da multiplicação entre ${value1.value} e ${value2.value} é ${multiplication(x,y,z)}. Tente outras operações!`);
                 break;
 
             //potentiation listener
             case 'potentiation':
-                try {
-                    if (Number.isInteger(x) === false || Number.isInteger(y) === false || x < 0 || y < 0) {
-                        throw new Error(`[potentiate] Impossible to raise ${a} to ${b}.`);
-                    }
-                    result.textContent =(`O valor de ${value1.value} elevado a  ${value2.value} é ${potentiation(x,y,z,w)}. Tente outras operações!`);
-                } catch (error) {
-                    result.textContent =(error.message);
-                }  
+                if (Number.isInteger(x) === false || Number.isInteger(y) === false || x < 0 || y < 0) {
+                    throw new Error(`[potentiate] Impossible to raise ${x} to ${y}.`);
+                }
+                result.textContent =(`O valor de ${value1.value} elevado a  ${value2.value} é ${potentiation(x,y,z,w)}. Tente outras operações!`);
                 break;
                 
             //division listener  
             default:
-                try {
-                    if (Number.isInteger(x) === false || Number.isInteger(y) === false || x < 0 || y < 0) {
-                        throw new Error(`[divide] Impossible to divice ${a} to ${b}.`);
-                    } else if (y === 0) {
-                        throw new Error(`[divide] Division by zero`);
-                    }
-                    result.textContent =(`O valor da divisão entre ${value1.value} e ${value2.value} é ${division(x,y,w)} e o seu resto é ${rest(x,y,w)}. Tente outras operações!`);
-                break;
-                } catch (error) {
-                    result.textContent =(error.message);
-                }         
+                if (Number.isInteger(x) === false || Number.isInteger(y) === false || x < 0 || y < 0) {
+                    throw new Error(`[divide] Impossible to divice ${x} to ${y}.`);
+                } else if (y === 0) {
+                    throw new Error(`[divide] Division by zero`);
+                }
+                result.textContent =(`O valor da divisão entre ${value1.value} e ${value2.value} é ${division(x,y,w)} e o seu resto é ${rest(x,y,w)}. Tente outras operações!`);  
         }
-        
+    
+    //message error
     } catch (error) {
-        window.alert(error.message);
+        result.textContent = (error.message);
     }
 }
 
